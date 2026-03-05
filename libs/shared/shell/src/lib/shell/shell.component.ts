221e38afb30214a37933733dd4edf0b2aaa71e23
Jeff Peterson <gpeterso@umn.edu>
2026-03-05 11:50:39 -0600

    Add shell lib
    Upgrade nx (chose not to upgrade ts and angular when prompted):
    `npx nx migrate 22.2.0 --interactive`
    `npm i`
    `npx nx migrate --run-migrations`
    
    Create shell lib:
    `npx nx g @nx/angular:lib --name=shell --directory=libs/shared/shell --unitTestRunner=vitest`

---
+++ libs/shared/shell/src/lib/shell/shell.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-shell',
+  standalone: true,
+  imports: [],
+  templateUrl: './shell.component.html',
+  styleUrl: './shell.component.css',
+})
+export class ShellComponent {}
