cc8a0c2e1c6742e77d4bd175cb17a00dcd9f78ee
Jeff Peterson <gpeterso@umn.edu>
2026-03-13 17:04:28 -0500

    Add proxy config to twincities view
    Seems to work as expected...

--- libs/vendor/custom-module/proxy/proxy.const.mjs
+++ libs/vendor/custom-module/proxy/proxy.const.mjs
@@ -1,7 +1,9 @@
 //set the url of the server you want to test your code with and start the development server using the following command:
-// ng serve --proxy-config ./proxy/proxy.conf.mjs   
+// ng serve --proxy-config ./proxy/proxy.conf.mjs
 const environments = {
-    'example': 'https://myPrimoVE.com',
-  }
-  
-  export const PROXY_TARGET = environments['example']; 
+  'example': 'https://myPrimoVE.com',
+  'sandbox': 'https://umn-psb.primo.exlibrisgroup.com',
+  'production': 'https://primo.lib.umn.edu',
+}
+
+export const PROXY_TARGET = environments['sandbox'];
