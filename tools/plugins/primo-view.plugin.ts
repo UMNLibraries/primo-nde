89df5278394260c3715c110749578a765f85a2ae
Jeff Peterson <gpeterso@umn.edu>
2026-03-15 15:23:03 -0500

    Add nx plugin for primo views
    - enables inheriting assets from the base view, which can be overridden
      in the view project
    - centralizes webpack config for views
    - sets implicit dependency on the base-view lib

---
+++ tools/plugins/primo-view.plugin.ts
@@ -0,0 +1,46 @@
+import { CreateNodesV2, createNodesFromFiles } from '@nx/devkit';
+import { dirname } from 'path';
+
+export const createNodes: CreateNodesV2 = [
+  'views/*/project.json',
+  async (configFiles, options, context) => {
+    return await createNodesFromFiles(
+      (projectConfigFile) => {
+        const projectRoot = dirname(projectConfigFile);
+
+        return {
+          projects: {
+            [projectRoot]: {
+              implicitDependencies: ['base-view'],
+              targets: {
+                build: {
+                  options: {
+                    customWebpackConfig: {
+                      path: 'tools/webpack/webpack.config.js',
+                    },
+                    assets: [
+                      // views inherit assets from the base-view
+                      {
+                        glob: '**/*',
+                        input: 'libs/shared/base-view/src/assets',
+                        output: 'assets',
+                      },
+                      {
+                        glob: '**/*',
+                        input: `${projectRoot}/src/assets`,
+                        output: 'assets',
+                      },
+                    ],
+                  },
+                },
+              },
+            },
+          },
+        };
+      },
+      configFiles,
+      options,
+      context
+    );
+  },
+];
