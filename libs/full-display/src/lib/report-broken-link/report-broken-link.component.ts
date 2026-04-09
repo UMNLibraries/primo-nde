4e61f6bd548dc40e42ee398d80ff4dc9eae418f5
Jeff Peterson <gpeterso@umn.edu>
2026-04-09 13:14:21 -0500

    Clean up broken link report component
    Not using external template or styles

--- libs/full-display/src/lib/report-broken-link/report-broken-link.component.ts
+++ libs/full-display/src/lib/report-broken-link/report-broken-link.component.ts
@@ -13,18 +13,20 @@ import { MatIconModule } from '@angular/material/icon';
   standalone: true,
   selector: 'umn-report-broken-link',
   imports: [MatButtonModule, MatIconModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `<a mat-button target="_blank" [href]="targetUrl()"
     ><mat-icon>build</mat-icon><span><ng-content /></span
   ></a>`,
-  styleUrl: './report-broken-link.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReportBrokenLinkComponent {
   private document = inject(DOCUMENT);
+
   // base url for problem report form
   url = input.required<string>();
-  // name of the parameter to pass the user's current location/page
+
+  // name of the parameter to pass the user's current location/page (optional)
   locationParam = input<string>();
+
   targetUrl = computed(() => {
     const base = new URL(this.url());
     const param = this.locationParam();
