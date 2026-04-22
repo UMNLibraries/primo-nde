62aa21852461a78e97cf3ed24a7466d15e5c6e03
Jeff Peterson <gpeterso@umn.edu>
2026-04-22 11:58:39 -0500

    Add PROXY_TARGET env var option

--- libs/vendor/custom-module/proxy/proxy.const.mjs
+++ libs/vendor/custom-module/proxy/proxy.const.mjs
@@ -1,9 +1,9 @@
 //set the url of the server you want to test your code with and start the development server using the following command:
 // ng serve --proxy-config ./proxy/proxy.conf.mjs
 const environments = {
-  'example': 'https://myPrimoVE.com',
-  'sandbox': 'https://umn-psb.primo.exlibrisgroup.com',
-  'production': 'https://primo.lib.umn.edu',
-}
+  example: 'https://myPrimoVE.com',
+  sandbox: 'https://umn-psb.primo.exlibrisgroup.com',
+  production: 'https://primo.lib.umn.edu',
+};
 
-export const PROXY_TARGET = environments['sandbox'];
+export const PROXY_TARGET = process.env.PROXY_TARGET ?? environments['sandbox'];
