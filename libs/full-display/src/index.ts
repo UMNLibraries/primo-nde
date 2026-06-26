f262c252a50b367297396ae15e7f9d301de718f4
Jeff Peterson <gpeterso@umn.edu>
2026-06-26 14:50:57 -0500

    Rename problem report component dir

--- libs/full-display/src/index.ts
+++ libs/full-display/src/index.ts
@@ -1,2 +1,2 @@
 export * from './lib/filter-request-pickup-locations.component';
-export * from './lib/report-broken-link/problem-report-link.component';
+export * from './lib/problem-report-link/problem-report-link.component';
