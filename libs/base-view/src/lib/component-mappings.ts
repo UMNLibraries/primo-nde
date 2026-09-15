fb588a56ecca3d83a92424acf54b3104f144afff
Jeff Peterson <gpeterso@umn.edu>
2026-09-15 09:00:27 -0500

    Remove hathi-trust from base view
    Planning to use the add-on going forward...

--- libs/base-view/src/lib/component-mappings.ts
+++ libs/base-view/src/lib/component-mappings.ts
@@ -2,7 +2,6 @@ import type { Type } from '@angular/core';
 import { FilterRequestPickupLocationsComponent } from '@umn-nde/filter-pickup-locations';
 import { FilterCampusCollectionsComponent } from '@umn-nde/filter-campus-collections';
 import { ExcludeBeyondComponent } from '@umn-nde/exclude-beyond-filter';
-import { HathiTrustComponent } from '@umn-nde/hathi-truse';
 
 type NdeSelector = `nde-${string}`;
 
@@ -13,6 +12,5 @@ export const selectorComponentMap = new Map<NdeSelector, Type<object>>([
     FilterCampusCollectionsComponent,
   ],
   ['nde-requests-after', FilterRequestPickupLocationsComponent],
-  ['nde-online-availability-before', HathiTrustComponent],
   ['nde-search-filters-side-nav-top', ExcludeBeyondComponent],
 ]);
